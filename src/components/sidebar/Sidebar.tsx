ff1e1b5108197e02510c5818ab23094ef64c9ca3
Hayate <ha08ya@gmail.com>
2023-07-25 02:43:43 +0900

    feat: :construction: #11 Add icon in Sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -4,8 +4,19 @@ import './Sidebar.scss';
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="sidebar__left"></div>
-      <div className="sidebar__right"></div>
+      <div className="sidebar__left">
+        <div className="sever-icon">
+          <img src="./logo192.png" alt="ロゴ" />
+        </div>
+        <div className="sever-icon">
+          <img src="./logo192.png" alt="ロゴ" />
+        </div>
+      </div>
+      <div className="sidebar__right">
+        <div className="sidebar__top">
+          <h3>discord</h3>
+        </div>
+      </div>
     </div>
   );
 };
