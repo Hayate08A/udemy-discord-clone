51edd4262781f567c8a2cc177ff0446892f07fe8
Hayate <ha08ya@gmail.com>
2024-03-13 02:14:02 +0900

    feat: :construction: #34 google login

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import './Login.scss';
 import { Button } from '@mui/material';
 import iconDiscord from '../../images/discordIcon.png';
+import { signInWithPopup } from 'firebase/auth';
+import { auth, provider } from '../../firebase';
 
 function Login() {
+  const signIn = () => {
+    signInWithPopup(auth, provider).catch((err: Error) => {
+      alert(err.message);
+    });
+  };
   return (
     <div className="login">
       <div className="login__logo">
         <img src={iconDiscord} alt="logo" />
       </div>
-      <Button>ログイン</Button>
+      <Button onClick={signIn}>ログイン</Button>
     </div>
   );
 }
