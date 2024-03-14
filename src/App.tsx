bf283e8f86054091d9144055c01a5d16e27c54d5
Hayate <ha08ya@gmail.com>
2024-03-15 02:58:54 +0900

    feat: :heavy_plus_sign: #37 ~ #41 react-error-boundary

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Login from './components/login/Login';
 import { useAppDispatch, useAppSelector } from './app/hooks';
 import { auth } from './firebase';
 import { login, logout } from './features/userSlice';
+import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorFallBack } from './utils/ErrorFallBack';
 
 function App() {
   const user = useAppSelector((state) => state.user);
@@ -35,8 +37,9 @@ function App() {
     <div className="App">
       {user ? (
         <>
-          {/* chat */}
-          <Sidebar />
+          <ErrorBoundary FallbackComponent={ErrorFallBack}>
+            <Sidebar />
+          </ErrorBoundary>
           <Chat />
         </>
       ) : (
